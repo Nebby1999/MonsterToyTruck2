12f461e560aab73d34d4b3a40f660f576a044ed8
Nebby1999 <nebby131999@gmail.com>
2022-05-10 17:58:29 -0400

    Nebby.UnityUtils.Runtime & Nebby.UnityUtils.Editor

--- MTT2-Project/Assets/Scripts/Nebby.UnityUtils/Runtime/DontDestroyOnLoad.cs
+++ MTT2-Project/Assets/Scripts/Nebby.UnityUtils/Runtime/DontDestroyOnLoad.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-namespace Nebby.UnityUtils{
+
+namespace Nebby.UnityUtils
+{
     public class DontDestroyOnLoad : MonoBehaviour
     {
         private void Awake()
--- MTT2-Project/Assets/Scripts/Nebby.UnityUtils/Runtime/SerializableSystemType.cs
+++ MTT2-Project/Assets/Scripts/Nebby.UnityUtils/Runtime/SerializableSystemType.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
 
-namespace Nebby.UnityUtils.Serialization
+namespace Nebby.UnityUtils
 {
     [Serializable]
     public struct SerializableSystemType
