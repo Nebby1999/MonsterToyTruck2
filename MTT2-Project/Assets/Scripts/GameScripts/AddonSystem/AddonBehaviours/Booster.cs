62e7b06f9c3b06d16463ba33d000f84bf1aee07d
Nebby <nebby131999@gmail.com>
2022-05-26 19:11:36 -0400

    Booster pog!

--- MTT2-Project/Assets/Scripts/GameScripts/AddonSystem/AddonBehaviours/Booster.cs
+++ MTT2-Project/Assets/Scripts/GameScripts/AddonSystem/AddonBehaviours/Booster.cs
@@ -7,6 +7,10 @@ namespace MTT2.Addons
     public class Booster : AddonBehaviourBase
     {
         bool value;
+        public float baseBoostCoefficient;
+        public float maxBoostCoefficient;
+        public float coefficientGain;
+        float coefficient;
         public override void AddonTrigger(InputAction.CallbackContext context)
         {
             value = context.ReadValueAsButton();
@@ -14,10 +18,26 @@ namespace MTT2.Addons
 
         public void FixedUpdate()
         {
-            Debug.Log(value);
+            CalculateCoefficient();
             if(value)
             {
-                TruckController.RigidBody2d.AddForceAtPosition(TruckController.transform.rotation.eulerAngles * 100, transform.position, ForceMode2D.Impulse);
+                TruckController.RigidBody2d.AddForceAtPosition(Vector2.right * coefficient, transform.position, ForceMode2D.Impulse);
+            }
+        }
+
+        private void CalculateCoefficient()
+        {
+            if(value)
+            {
+                coefficient += coefficientGain * Time.fixedDeltaTime;
+                if(coefficient > maxBoostCoefficient)
+                    coefficient = maxBoostCoefficient;
+            }
+            else
+            {
+                coefficient -= coefficientGain * Time.fixedDeltaTime;
+                if (coefficient < baseBoostCoefficient)
+                    coefficient = baseBoostCoefficient;
             }
         }
     }
