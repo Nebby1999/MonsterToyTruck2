6e287ee667753700949b14f038e1f5f2f04915c5
Nebby <nebby131999@gmail.com>
2022-05-28 17:20:25 -0400

    no se wn

---
+++ MTT2-Project/Assets/Scripts/GameScripts/RandomSpriteAssigner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using Nebby.CSharpUtils;
+
+namespace MTT2
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class RandomSpriteAssigner : MonoBehaviour
+    {
+        public Sprite[] sprites = Array.Empty<Sprite>();
+        public UnityEvent onSpriteAssigned;
+
+        private SpriteRenderer spriteRenderer;
+
+        private void Awake()
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Start()
+        {
+            if(sprites.TryGetRandomElement(out Sprite sprite))
+            {
+                spriteRenderer.sprite = sprite;
+                onSpriteAssigned.Invoke();
+            }
+        }
+    }
+}
